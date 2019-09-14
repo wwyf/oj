ce7307c7cdaed9b4ee777bf9bb80281effe2caf0
wyf-book <walkeryf@yeah.net>
2019-09-14 15:10:57 +0800

    783

---
+++ leetcode/783.minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=783 lang=cpp
+ *
+ * [783] Minimum Distance Between BST Nodes
+ *
+ * https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+ *
+ * algorithms
+ * Easy (50.95%)
+ * Likes:    411
+ * Dislikes: 116
+ * Total Accepted:    40K
+ * Total Submissions: 78.5K
+ * Testcase Example:  '[4,2,6,1,3,null,null]'
+ *
+ * Given a Binary Search Tree (BST) with the root node root, return the minimum
+ * difference between the values of any two different nodes in the tree.
+ * 
+ * Example :
+ * 
+ * 
+ * Input: root = [4,2,6,1,3,null,null]
+ * Output: 1
+ * Explanation:
+ * Note that root is a TreeNode object, not an array.
+ * 
+ * The given tree [4,2,6,1,3,null,null] is represented by the following
+ * diagram:
+ * 
+ * ⁠         4
+ * ⁠       /   \
+ * ⁠     2      6
+ * ⁠    / \    
+ * ⁠   1   3  
+ * 
+ * while the minimum difference in this tree is 1, it occurs between node 1 and
+ * node 2, also between node 3 and node 2.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The size of the BST will be between 2 and 100.
+ * The BST is always valid, each node's value is an integer, and each node's
+ * value is different.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int prev = INT_MIN;
+    int minium = INT_MAX;
+    int minDiffInBST(TreeNode* root) {
+        if (root->left)  minDiffInBST(root->left);
+        if (prev != INT_MIN)
+            minium = min(root->val - prev, minium);
+        prev = root->val;
+        if (root->right) minDiffInBST(root->right);
+        return minium;
+    }
+    
+};
