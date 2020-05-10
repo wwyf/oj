dd999342c1dd6c5c33c562adc02d73b0ad20d36f
wwyf <w.y.ff@163.com>
2020-05-10 10:17:08 +0800

    add  917

---
+++ leetcode/917.仅仅反转字母.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=917 lang=cpp
+ *
+ * [917] 仅仅反转字母
+ */
+
+/*
+
+太简单了
+拿一个栈存放就好。
+*/
+
+
+// @lc code=start
+#include <cctype>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    string reverseOnlyLetters(string S) {
+        stack<char> rc;
+        for (auto c : S){
+            if (isalpha(c)){
+                rc.push(c);
+            }
+        }
+        for (auto & c : S){
+            if (isalpha(c)){
+                c = rc.top();
+                rc.pop();
+            }
+        }
+        return S;
+    }
+};
+// @lc code=end
+
