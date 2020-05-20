b1d85770ecab0ebd5fa371e1ea221c4172733ec5
wwyf <w.y.ff@163.com>
2020-05-20 12:39:30 +0800

    add 877

---
+++ leetcode/877.石子游戏.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=cpp
+ *
+ * [877] 石子游戏
+ */
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// @lc code=start
+const int maxLen = 501;
+class Solution {
+public:
+    int winN[maxLen][maxLen];
+    bool isWin[maxLen][maxLen];
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+// @lc code=end
+
