a7182a9c853573816e506abbc9d7d11c299b55aa
wwyf <w.y.ff@163.com>
2020-07-08 10:57:40 +0800

    update 146

--- leetcode/146.lru缓存机制.cpp
+++ leetcode/146.lru缓存机制.cpp
@@ -5,77 +5,96 @@
  */
 
 #include <vector>
+#include <map>
+#include <unordered_map>
 using namespace std;
 // @lc code=start
-struct Item{
+struct Node{
+    Node * prev;
+    Node * next;
     int key;
     int value;
-    int oldIndex;
+    Node(){
+        prev = NULL;
+        next = NULL;
+        key = 0;
+        value = 0;
+    }
 };
 
 class LRUCache {
 public:
-    vector<Item> cache;
-    int c;
+    Node * head;
+    Node * tail;
+    int capacity;
     int size;
+    unordered_map<int, Node *> k2n;
     LRUCache(int capacity) {
-        cache = vector<Item>(capacity);
-        this->c = capacity;
+        head = new Node();
+        tail = new Node();
+        head->next = tail;
+        tail->prev = head;
+        this->capacity = capacity;
         this->size = 0;
     }
-    
+    void access(Node * n){
+        // bug!!!
+        if (n == head->next){
+            return;
+        }
+        // put n to head
+        Node * tmpNext = n->next;
+        Node * tmpPrev = n->prev;
+        // next
+        n->next = head->next;
+        head->next = n;
+        tmpPrev->next = tmpNext;
+        // prev
+        n->prev = head;
+        n->next->prev = n;
+        tmpNext->prev = tmpPrev;
+    }
     int get(int key) {
-        int findIndex = this->find(key);
-        if (findIndex == -1){
+        auto findNodeIter = k2n.find(key);
+        if (findNodeIter == k2n.end()){
+            // can not find key in k2n
             return -1;
-        } else{
-            return cache[findIndex].value;
+        } else {
+            this->access(findNodeIter->second);
+            return findNodeIter->second->value;
         }
-    }
-    
-    int find(int key){
-        int findIndex = -1;
-        for (int i = 0; i < this->size; i++){
-            if (cache[i].key == key){
-                findIndex = i;
-            }
-            cache[i].oldIndex++;
-        }
-        if (findIndex != -1){
-            cache[findIndex].oldIndex = 0;
-        }
-        return findIndex;
-    }
-    int findOldest(){
-        int oldIndex = 0;
-        int oldI = 0;
-        for (int i = 0; i < this->size; i++){
-            if (cache[i].oldIndex > oldIndex){
-                oldIndex = cache[i].oldIndex;
-                oldI = i;
-            }
-        }
-        return oldI;
     }
     void put(int key, int value) {
-        int findIndex = this->find(key);
-        if (findIndex != -1){
-            cache[findIndex].value = value;
-        } else {
-            // not found
-            if (this->size < this->c){
-                cache[this->size].key = key;
-                cache[this->size].value = value;
-                cache[this->size].oldIndex = 0;
+        auto findNodeIter = k2n.find(key);
+        if (findNodeIter == k2n.end()){
+            // can not find key in k2n
+            // must put new node
+            if (this->size < this->capacity){
+                // directly put new node to head
+                Node * newNode = new Node();
+                newNode->key = key;
+                newNode->value = value;
+                newNode->next = head->next;
+                newNode->prev = head;
+                head->next = newNode;
+                newNode->next->prev = newNode;
                 this->size++;
+                k2n[key] = newNode;
             } else {
-                // find oldest element
-                int oldI = findOldest();
-                cache[oldI].key = key;
-                cache[oldI].value = value;
-                cache[oldI].oldIndex = 0;
+                // move tail to head and modify
+                Node * tailNode = tail->prev;
+                // delete from map k2n
+                k2n.erase(tailNode->key);
+                this->access(tailNode);
+                tailNode->key = key;
+                tailNode->value = value;
+                k2n[key] = tailNode;
             }
+        } else {
+            this->access(findNodeIter->second);
+            findNodeIter->second->value = value;
         }
+
     }
 };
 
