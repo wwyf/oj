f1b21a45e986519415e1db9a678aeb2c85dab786
wwyf <w.y.ff@163.com>
2020-07-16 09:47:22 +0800

    update 292

--- leetcode/292.nim-游戏.cpp
+++ leetcode/292.nim-游戏.cpp
@@ -4,6 +4,13 @@
  * [292] Nim 游戏
  */
 
+/*
+
+思路虽然很简单，但是其中可以用到的dp知识可以学学！！
+https://leetcode-cn.com/problems/nim-game/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-54/
+
+*/
+
 // @lc code=start
 class Solution {
 public:
