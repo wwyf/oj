9dec3cbbde2c1bff24f85aa7619ca5bb405e8a05
wwyf <w.y.ff@163.com>
2020-10-05 09:48:50 +0800

    add 617

---
+++ leetcode/617.合并二叉树.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=cpp
+ *
+ * [617] 合并二叉树
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == nullptr && t2 == nullptr){
+            return nullptr;
+        }
+        if (t1 == nullptr){
+            return t2;
+        }
+        if (t2 == nullptr){
+            return t1;
+        }
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
+// @lc code=end
+
