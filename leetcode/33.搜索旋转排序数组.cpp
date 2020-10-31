058c1386025b18465198d7987b9f1d82b3d3e848
wwyf <w.y.ff@163.com>
2020-10-31 12:12:26 +0800

    add 33

---
+++ leetcode/33.搜索旋转排序数组.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=cpp
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// @lc code=start
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = (int)nums.size();
+        if (!n) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
